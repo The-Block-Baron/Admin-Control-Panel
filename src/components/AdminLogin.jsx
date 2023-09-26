4969b574c2cd745cf95a4fad6a4b8353d68dcbe3
Diego Prieto <prieto.diego@proton.me>
2023-09-26 07:32:46 +0200

    Applied design to AdminLogin component

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,11 +1,43 @@
 // src/components/AdminLogin.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import { Title, LoginContainer, Container, Form, Button, Input } from './StyledAdminLogin';
 
 const AdminLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you will handle the form submission and connect to the API
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>
-      <h1>Admin Login</h1>
-    </div>
+    <LoginContainer>
+      <Title>Admin Login</Title>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+        <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Button type="submit">Login</Button>
+        </Form>
+      </Container>
+    </LoginContainer>
   );
 };
 
